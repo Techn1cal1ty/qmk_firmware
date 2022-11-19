0b5906deab5f462d04a577e55ac8508a1b979bfe
James Braun <james.braun8192@gmail.com>
2022-11-19 16:08:20 -0600

    Central 1.5u key rework
    Changed functionality of the central keys to allow better access to '{' and '}'. Expanded access to '<' and '>' using OSL

--- keyboards/customergodoxv1/keymaps/default/keymap.c
+++ keyboards/customergodoxv1/keymaps/default/keymap.c
@@ -47,17 +47,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 LAYOUT_void_ergo(
-    KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_GRV,             KC_MINS,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_LBRC,            KC_RBRC,  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
-    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_LPRN,            KC_RPRN,  KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                   KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-    KC_LCTL,  TT(1),    TT(2),    KC_LGUI,  KC_LALT,                                                    TT(2),    TT(1),    KC_RALT,  KC_APP,   KC_RCTL,
-                                                      KC_LEFT,  KC_RGHT,            KC_DOWN,  KC_UP,
-                                            KC_BSPC,  KC_SPC,   KC_HOME,            KC_PGUP,  KC_SPC,   KC_ENT,
-                                                                KC_END,             KC_PGDN
+            KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_GRV,             KC_MINS,    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,
+            KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_LCBR,            KC_RCBR,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
+            KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_LPRN,            KC_RPRN,    KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+MT(KC_LSFT, OSL(3)),  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+            KC_LCTL,  TT(1),    TT(2),    KC_LGUI,  KC_LALT,                                                      TT(2),    TT(1),    KC_RALT,  KC_APP,   KC_RCTL,
+                                                              KC_LEFT,  KC_RGHT,            KC_DOWN,  KC_UP,
+                                                    KC_BSPC,  KC_SPC,   KC_HOME,            KC_PGUP,  KC_SPC,   KC_ENT,
+                                                                        KC_END,             KC_PGDN
 ),
 LAYOUT_void_ergo(
-    KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,              KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,
+    TO(0),    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,              KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,
     KC_TRNS,  KC_EXLM,  KC_AT,    KC_LCBR,  KC_RCBR,  KC_PIPE,  KC_TRNS,            KC_TRNS,  KC_UP,    KC_7,     KC_8,     KC_9,     KC_ASTR,  KC_TRNS,
     KC_TRNS,  KC_HASH,  KC_DLR,   KC_LPRN,  KC_RPRN,  KC_GRV,   KC_TRNS,            KC_TRNS,  KC_DOWN,  KC_4,     KC_5,     KC_6,     KC_PLUS,  KC_TRNS,
     KC_TRNS,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_TILD,                                KC_AMPR,  KC_1,     KC_2,     KC_3,     KC_BSLS,  KC_TRNS,
@@ -75,6 +75,16 @@ LAYOUT_void_ergo(
                                                       KC_NO,    KC_NO,              KC_NO,    KC_NO,
                                             TO(0),    TO(0),    KC_NO,              KC_NO,    TO(0),    TO(0),
                                                                 KC_NO,              KC_NO
+),
+LAYOUT_void_ergo(
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LBRC,            KC_RBRC,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LT,              KC_GT,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                                                    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+                                                      KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,
+                                            KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
+                                                                KC_TRNS,            KC_TRNS
 )
 };
 
